384b3a4eb024fb1444e9b6214c8f15013d728f1c
BoD <BoD@JRAF.org>
2014-06-15 15:22:06 +0200

    Bump to 1.8.0.

--- src/main/java/org/jraf/androidcontentprovidergenerator/Constants.java
+++ src/main/java/org/jraf/androidcontentprovidergenerator/Constants.java
@@ -25,16 +25,8 @@
 package org.jraf.androidcontentprovidergenerator;
 
 public class Constants {
-
-    /**
-     * Current version of this tool.<br/>
-     * <em>Do not forget to update the value in the {@code pom.xml} file when updating this value.</em>
-     */
-    public static final String VERSION = "1.7.2";
-
     // TODO: For the next version, use a single digit, instead of a version number with several parts
     // TODO: which doesn't make sense for the version number of a syntax.
     public static final String SYNTAX_VERSION = "1.7";
     public static final String TAG = "";
-
 }
