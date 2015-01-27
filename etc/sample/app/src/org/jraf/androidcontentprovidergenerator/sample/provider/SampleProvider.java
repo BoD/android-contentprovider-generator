fade08cbbff00f0872acc6c3710c0e45fb45837d
Steve Lhomme <robux4@gmail.com>
2015-01-27 15:51:52 +0100

    use a BaseContentProvider in base to factorize some code when using multiple databases

--- etc/sample/app/src/org/jraf/androidcontentprovidergenerator/sample/provider/SampleProvider.java
+++ etc/sample/app/src/org/jraf/androidcontentprovidergenerator/sample/provider/SampleProvider.java
@@ -24,24 +24,17 @@
  */
 package org.jraf.androidcontentprovidergenerator.sample.provider;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 
-import android.content.ContentProvider;
-import android.content.ContentProviderOperation;
-import android.content.ContentProviderResult;
 import android.content.ContentValues;
-import android.content.OperationApplicationException;
 import android.content.UriMatcher;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
-import android.provider.BaseColumns;
 import android.util.Log;
 
 import org.jraf.androidcontentprovidergenerator.sample.BuildConfig;
+import org.jraf.androidcontentprovidergenerator.sample.provider.base.BaseContentProvider;
 import org.jraf.androidcontentprovidergenerator.sample.provider.company.CompanyColumns;
 import org.jraf.androidcontentprovidergenerator.sample.provider.person.PersonColumns;
 import org.jraf.androidcontentprovidergenerator.sample.provider.personteam.PersonTeamColumns;
@@ -49,7 +42,7 @@ import org.jraf.androidcontentprovidergenerator.sample.provider.product.ProductC
 import org.jraf.androidcontentprovidergenerator.sample.provider.serialnumber.SerialNumberColumns;
 import org.jraf.androidcontentprovidergenerator.sample.provider.team.TeamColumns;
 
-public class SampleProvider extends ContentProvider {
+public class SampleProvider extends BaseContentProvider {
     private static final String TAG = SampleProvider.class.getSimpleName();
 
     private static final boolean DEBUG = BuildConfig.DEBUG;
@@ -59,9 +52,6 @@ public class SampleProvider extends ContentProvider {
 
     public static final String AUTHORITY = "org.jraf.androidcontentprovidergenerator.sample.provider";
     public static final String CONTENT_URI_BASE = "content://" + AUTHORITY;
-
-    public static final String QUERY_NOTIFY = "QUERY_NOTIFY";
-    public static final String QUERY_GROUP_BY = "QUERY_GROUP_BY";
 
     private static final int URI_TYPE_COMPANY = 0;
     private static final int URI_TYPE_COMPANY_ID = 1;
@@ -100,29 +90,14 @@ public class SampleProvider extends ContentProvider {
         URI_MATCHER.addURI(AUTHORITY, TeamColumns.TABLE_NAME + "/#", URI_TYPE_TEAM_ID);
     }
 
-    protected SampleSQLiteOpenHelper mSampleSQLiteOpenHelper;
+    @Override
+    protected SQLiteOpenHelper createSqLiteOpenHelper() {
+        return SampleSQLiteOpenHelper.getInstance(getContext());
+    }
 
     @Override
-    public boolean onCreate() {
-        if (DEBUG) {
-            // Enable logging of SQL statements as they are executed.
-            try {
-                Class<?> sqliteDebugClass = Class.forName("android.database.sqlite.SQLiteDebug");
-                Field field = sqliteDebugClass.getDeclaredField("DEBUG_SQL_STATEMENTS");
-                field.setAccessible(true);
-                field.set(null, true);
-
-                // Uncomment the following block if you also want logging of execution time (more verbose)
-                // field = sqliteDebugClass.getDeclaredField("DEBUG_SQL_TIME");
-                // field.setAccessible(true);
-                // field.set(null, true);
-            } catch (Throwable t) {
-                if (DEBUG) Log.w(TAG, "Could not enable SQLiteDebug logging", t);
-            }
-        }
-
-        mSampleSQLiteOpenHelper = SampleSQLiteOpenHelper.getInstance(getContext());
-        return true;
+    protected boolean hasDebug() {
+        return DEBUG;
     }
 
     @Override
@@ -166,132 +141,37 @@ public class SampleProvider extends ContentProvider {
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         if (DEBUG) Log.d(TAG, "insert uri=" + uri + " values=" + values);
-        String table = uri.getLastPathSegment();
-        long rowId = mSampleSQLiteOpenHelper.getWritableDatabase().insertOrThrow(table, null, values);
-        if (rowId == -1) return null;
-        String notify;
-        if (rowId != -1 && ((notify = uri.getQueryParameter(QUERY_NOTIFY)) == null || "true".equals(notify))) {
-            getContext().getContentResolver().notifyChange(uri, null);
-        }
-        return uri.buildUpon().appendEncodedPath(String.valueOf(rowId)).build();
+        return super.insert(uri, values);
     }
 
     @Override
     public int bulkInsert(Uri uri, ContentValues[] values) {
         if (DEBUG) Log.d(TAG, "bulkInsert uri=" + uri + " values.length=" + values.length);
-        String table = uri.getLastPathSegment();
-        SQLiteDatabase db = mSampleSQLiteOpenHelper.getWritableDatabase();
-        int res = 0;
-        db.beginTransaction();
-        try {
-            for (ContentValues v : values) {
-                long id = db.insert(table, null, v);
-                db.yieldIfContendedSafely();
-                if (id != -1) {
-                    res++;
-                }
-            }
-            db.setTransactionSuccessful();
-        } finally {
-            db.endTransaction();
-        }
-        String notify;
-        if (res != 0 && ((notify = uri.getQueryParameter(QUERY_NOTIFY)) == null || "true".equals(notify))) {
-            getContext().getContentResolver().notifyChange(uri, null);
-        }
-
-        return res;
+        return super.bulkInsert(uri, values);
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         if (DEBUG) Log.d(TAG, "update uri=" + uri + " values=" + values + " selection=" + selection + " selectionArgs=" + Arrays.toString(selectionArgs));
-        QueryParams queryParams = getQueryParams(uri, selection, null);
-        int res = mSampleSQLiteOpenHelper.getWritableDatabase().update(queryParams.table, values, queryParams.selection, selectionArgs);
-        String notify;
-        if (res != 0 && ((notify = uri.getQueryParameter(QUERY_NOTIFY)) == null || "true".equals(notify))) {
-            getContext().getContentResolver().notifyChange(uri, null);
-        }
-        return res;
+        return super.update(uri, values, selection, selectionArgs);
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         if (DEBUG) Log.d(TAG, "delete uri=" + uri + " selection=" + selection + " selectionArgs=" + Arrays.toString(selectionArgs));
-        QueryParams queryParams = getQueryParams(uri, selection, null);
-        int res = mSampleSQLiteOpenHelper.getWritableDatabase().delete(queryParams.table, queryParams.selection, selectionArgs);
-        String notify;
-        if (res != 0 && ((notify = uri.getQueryParameter(QUERY_NOTIFY)) == null || "true".equals(notify))) {
-            getContext().getContentResolver().notifyChange(uri, null);
-        }
-        return res;
+        return super.delete(uri, selection, selectionArgs);
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        String groupBy = uri.getQueryParameter(QUERY_GROUP_BY);
         if (DEBUG)
             Log.d(TAG, "query uri=" + uri + " selection=" + selection + " selectionArgs=" + Arrays.toString(selectionArgs) + " sortOrder=" + sortOrder
-                    + " groupBy=" + groupBy);
-        QueryParams queryParams = getQueryParams(uri, selection, projection);
-        projection = ensureIdIsFullyQualified(projection, queryParams.table, queryParams.idColumn);
-        Cursor res = mSampleSQLiteOpenHelper.getReadableDatabase().query(queryParams.tablesWithJoins, projection, queryParams.selection, selectionArgs, groupBy,
-                null, sortOrder == null ? queryParams.orderBy : sortOrder);
-        res.setNotificationUri(getContext().getContentResolver(), uri);
-        return res;
-    }
-
-    private String[] ensureIdIsFullyQualified(String[] projection, String tableName, String idColumn) {
-        if (projection == null) return null;
-        String[] res = new String[projection.length];
-        for (int i = 0; i < projection.length; i++) {
-            if (projection[i].equals(idColumn)) {
-                res[i] = tableName + "." + idColumn + " AS " + BaseColumns._ID;
-            } else {
-                res[i] = projection[i];
-            }
-        }
-        return res;
+                    + " groupBy=" + uri.getQueryParameter(QUERY_GROUP_BY) + " limit=" + uri.getQueryParameter(QUERY_LIMIT));
+        return super.query(uri, projection, selection, selectionArgs, sortOrder);
     }
 
     @Override
-    public ContentProviderResult[] applyBatch(ArrayList<ContentProviderOperation> operations) throws OperationApplicationException {
-        HashSet<Uri> urisToNotify = new HashSet<Uri>(operations.size());
-        for (ContentProviderOperation operation : operations) {
-            urisToNotify.add(operation.getUri());
-        }
-        SQLiteDatabase db = mSampleSQLiteOpenHelper.getWritableDatabase();
-        db.beginTransaction();
-        try {
-            int numOperations = operations.size();
-            ContentProviderResult[] results = new ContentProviderResult[numOperations];
-            int i = 0;
-            for (ContentProviderOperation operation : operations) {
-                results[i] = operation.apply(this, results, i);
-                if (operation.isYieldAllowed()) {
-                    db.yieldIfContendedSafely();
-                }
-                i++;
-            }
-            db.setTransactionSuccessful();
-            for (Uri uri : urisToNotify) {
-                getContext().getContentResolver().notifyChange(uri, null);
-            }
-            return results;
-        } finally {
-            db.endTransaction();
-        }
-    }
-
-    private static class QueryParams {
-        public String table;
-        public String tablesWithJoins;
-        public String idColumn;
-        public String selection;
-        public String orderBy;
-    }
-
-    private QueryParams getQueryParams(Uri uri, String selection, String[] projection) {
+    protected QueryParams getQueryParams(Uri uri, String selection, String[] projection) {
         QueryParams res = new QueryParams();
         String id = null;
         int matchedId = URI_MATCHER.match(uri);
@@ -394,13 +274,5 @@ public class SampleProvider extends ContentProvider {
             res.selection = selection;
         }
         return res;
-    }
-
-    public static Uri notify(Uri uri, boolean notify) {
-        return uri.buildUpon().appendQueryParameter(QUERY_NOTIFY, String.valueOf(notify)).build();
-    }
-
-    public static Uri groupBy(Uri uri, String groupBy) {
-        return uri.buildUpon().appendQueryParameter(QUERY_GROUP_BY, groupBy).build();
     }
 }
